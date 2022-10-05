2ea65d0bcc5dff595a0125bb54d7c49454618ad5
Shaik Sadath Basha <sadath.sa@gmail.com>
2022-10-05 23:07:31 +0530

    feat: Error and Loader components Content added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 const Error = () => (
-  <div>Error</div>
-);
+  <div className='w-full flex justify-center items-center'>
+    <h1 className='font-bold text-2xl text-white mt-2'> Loading ...</h1>
+  </div>
+)
 
-export default Error;
+export default Error
