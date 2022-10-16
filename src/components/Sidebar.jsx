c6a9a50f823d0c5d63d907cacd6eae33e4ede112
Shaik Sadath Basha <sadath.sa@gmail.com>
2022-10-17 01:21:41 +0530

    feat: created a sidebar with active links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,54 @@
-const Sidebar = () => (
-  <div>Sidebar</div>
-);
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { RiCloseLine } from 'react-icons/ri'
+import { HiOutlineMenu } from 'react-icons/hi'
 
-export default Sidebar;
+import { logo } from '../assets'
+import { links } from '../assets/constants'
+
+const NavLinks = ({ handleClick }) => (
+  <div className='mt-10'>
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className='flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400'
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className='w-6 h-6 mr-2' />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+)
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  return (
+    <>
+      <div className='md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624] '>
+        <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+        <NavLinks />
+      </div>
+
+      <div className='absolute md:hidden block top-6 right-3 cursor-pointer'>
+        {mobileMenuOpen ? (
+          <RiCloseLine onClick={() => setMobileMenuOpen(false)} className='h-6 w-6 text-white mr-2' />
+        ) : (
+          <HiOutlineMenu onClick={() => setMobileMenuOpen(true)} className='h-6 w-6 text-white mr-2' />
+        )}
+      </div>
+
+      <div
+        className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#484d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${
+          mobileMenuOpen ? 'left-0' : '-left-full'
+        } `}
+      >
+        <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  )
+}
+
+export default Sidebar
