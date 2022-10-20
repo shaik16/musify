a02fcfa6043c83be4a3042dc23b8b3ad27e360ed
Shaik Sadath Basha <sadath.sa@gmail.com>
2022-10-21 00:12:44 +0530

    update: bg gradient update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-[#060912]  via-[#213348] to-[#144d63]">
+      <div className="flex-1 flex flex-col bg-gradient-to-tr from-[#060912]  via-[#213348] to-[#144d63]">
         <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
