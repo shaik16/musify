7be6aa1ded5c657df7950ef354fa08ad4fdbe491
Shaik Sadath Basha <sadath.sa@gmail.com>
2022-12-24 02:08:53 +0530

    fix: render bug topcharts

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -27,7 +27,7 @@ const TopChartCard = ({
         <Link to={`/songs/${song?.key}`}>
           <p className="text-xl font-bold text-white">{song?.title}</p>
         </Link>
-        <Link to={`/artists/${song?.artists[0].adamid}`}>
+        <Link to={`/artists/${song?.artists && song.artists[0]?.adamid}`}>
           <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
         </Link>
       </div>
@@ -110,9 +110,9 @@ const TopPlay = () => {
               key={song?.key}
               style={{ width: '25%', height: 'auto' }}
               className="shadow-lg rounded-full animate-slideright">
-              <Link to={`/artists/${song?.artists[0].adamid}`}>
+              <Link to={`/artists/${song?.artists && song.artists[0].adamid}`}>
                 <img
-                  src={song?.images.background}
+                  src={song?.images?.background}
                   alt="artist-img"
                   className="rounded-full w-full object-cover"></img>
               </Link>
